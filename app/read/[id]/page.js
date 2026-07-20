829a9fc3024424ba9dc762ec765f6fddc41bb7ac
choiyui <choiyj220220@gmail.com>
2026-07-20 11:24:56 +0900

    feat: update Read component to display topic details dynamically

--- app/read/[id]/page.js
+++ app/read/[id]/page.js
@@ -1,10 +1,15 @@
 export default async function Read(props) {
   const { id } = await props.params;
+
+  const response = await fetch(`http://localhost:9999/topics/${id}`);
+  const topic = await response.json();
+
   console.log("Read Page 작동");
+
   return (
     <>
-      <h2>Read</h2>
-      <p>parameter:{id}</p>
+      <h2>{topic.title}</h2>
+      <p>{topic.message}</p>
     </>
   );
 }

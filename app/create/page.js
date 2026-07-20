ebf25a6fbcda315d99c1785570bc37a3fa8d2db9
choiyui <choiyj220220@gmail.com>
2026-07-20 11:50:51 +0900

    feat: implement Create form with input fields and submission handling

--- app/create/page.js
+++ app/create/page.js
@@ -1,8 +1,44 @@
+"use client";
 export default function Create() {
   console.log("Create Page 레이아웃 작동");
   return (
     <>
       <h3 style={styles.title}>Create Form</h3>
+      <form
+        action=""
+        onSubmit={e => {
+          e.preventDefault();
+          const title = e.target.title.value;
+          const message = e.target.message.value;
+        }}
+      >
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            placeholder="글 제목을 입력해주세요"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message
+          </label>
+          <textarea
+            name="message"
+            className="form-control"
+            id="message"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          입력
+        </button>
+      </form>
     </>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -66,6 +66,7 @@ export default async function RootLayout({ children }) {
         </nav>
         <main>
           {children}
+          <hr />
           <div className="d-flex gap-1">
             <Link className="btn btn-primary" href="/create">
               Create
